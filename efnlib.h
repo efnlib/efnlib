7208cb7f17e77180d6a50c08ab12a61114400d95
Efgeen <efgeen1998@gmail.com>
2024-06-21 00:11:25 +0200

    .

---
+++ efnlib.h
@@ -0,0 +1,10 @@
+#ifndef _EFNLIB_H
+#define _EFNLIB_H
+
+#include <efbitsrequired.h>
+#include <efcimgui_impl_dx9.h>
+#include <efcimgui_impl_win32.h>
+#include <eflog2.h>
+#include <efpopcount.h>
+
+#endif
